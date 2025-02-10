4cb933c372a28eb5be7df227eb691defd371acf0
rooockie <bayer.m@outlook.com>
2025-02-10 13:42:09 +0100

    First generic implementation
    Just some basic files needed for the html site

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add event listeners to the buttons
+document.getElementById('yes-button').addEventListener('click', function() {
+    alert('Thank you for your feedback! 😊');
+  });
+  
+  document.getElementById('no-button').addEventListener('click', function() {
+    alert('We will try to improve! 😢');
+  });
